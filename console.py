1fcc346aadd05347e3cc73bbafffa3abfe577f6b
BeygonK <kipkoechbeygon@gmail.com>
2024-03-12 01:49:55 -0700

    console

---
+++ console.py
@@ -0,0 +1,24 @@
+#!usr/bin/env bash
+"""This is a command line"""
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """This is a class"""
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit"""
+        print("")
+        return True
+
+    def emptyline(self):
+        """Does nothing"""
+        pass
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
