ea8020c47a7eb88666b204db8f2e8cf515974ac3
BeygonK <kipkoechbeygon@gmail.com>
2024-03-12 01:53:54 -0700

    console

--- console.py
+++ console.py
@@ -8,17 +8,18 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """Quit command to exit"""
+        """Quit command to exit the program"""
         return True
 
     def do_EOF(self, arg):
-        """EOF command to exit"""
+        """EOF command to exit the program"""
         print("")
         return True
 
     def emptyline(self):
         """Does nothing"""
         pass
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
