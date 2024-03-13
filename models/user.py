c832482ab05fada9226e3831f36a5474f9998d77
BeygonK <kipkoechbeygon@gmail.com>
2024-03-13 07:08:15 -0700

    fixed attributes

--- models/user.py
+++ models/user.py
@@ -6,11 +6,7 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """This is a class definition"""
-
-    def __init__(self, *args, **kwargs):
-        """Initializes User instance"""
-        super().__init__(*args, **kwargs)
-        self.email = kwargs.get('email', "")
-        self.password = kwargs.get('password', "")
-        self.first_name = kwargs.get('first_name', "")
-        self.last_name = kwargs.get('last_name', "")
+    email=""
+    password=""
+    first_name=""
+    last_name=""
