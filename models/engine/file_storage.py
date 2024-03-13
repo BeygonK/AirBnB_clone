052262d1057588d41df51da611eacc04a8e1973a
BeygonK <kipkoechbeygon@gmail.com>
2024-03-13 08:14:24 -0700

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,11 @@
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class FileStorage:
     """This is class definition"""
@@ -29,16 +34,22 @@ class FileStorage:
 
     def reload(self):
         """Loads from json"""
+        my_classes = {
+                'BaseModel': BaseModel,
+                'User': User,
+                'Place': Place,
+                'State': State,
+                'City': City,
+                'Amenity': Amenity,
+                'Review': Review
+                }
         try:
             with open(self.__file_path, 'r') as f:
                 data = json.load(f)
                 for key, value in data.items():
                     class_name, obj_id = key.split('.')
-                    if class_name == 'User':  # Check if the class name is User
-                        obj = User(**value)  # Create a User instance
-                    else:
-                        class_ = eval(class_name)  # Convert string to class object
-                        obj = class_(**value)
+                    class_ = my_classes[class_name]
+                    obj = class_(**value)  # Create a User instance
                     self.__objects[key] = obj
         except FileNotFoundError:
             pass
